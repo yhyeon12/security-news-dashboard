401b2594dd0ff62f881ac6bbb7f7f1e7a8f5b2ff
yhyeon12 <yhyeon00612@gmail.com>
2026-06-24 00:29:46 +0900

    Modified news, index, main

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ fetch("./data/news.json")
           <span class="tag">${news.category}</span>
           <h2>${news.title}</h2>
           <p>${news.summary}</p>
-          <a href="${news.link}" target="_blank">기사 보기</a>
+          <a href="${news.url}" target="_blank">기사 보기</a>
         </div>
       `;
     });
